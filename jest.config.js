8778949b21a4c8c976e5b74475605c3e46731a41
Maurício Braz Gonçalves <82123955+mauriciobraz@users.noreply.github.com>
2023-03-18 10:19:16 -0400

    test: remove jest

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'jest-expo',
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?@?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base)',
-  ],
-};
